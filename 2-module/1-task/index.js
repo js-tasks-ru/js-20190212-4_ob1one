a955788d925d5ad3780868d1104ab52ab9b3c0c2
Богдан Кононов <b.b.kononov@gmail.com>
2019-02-27 09:57:46 +0200

    added all solved tasks of 2 module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,15 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
-function clone (obj) {
+function clone(obj) {
+  let clonedObject = {};
+
+  for (let key in obj) {
+    clonedObject[key] = obj[key];
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      clonedObject[key] = clone(obj[key]);
+    }
+  }
+
+  return clonedObject;
 }
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -5,4 +5,27 @@
  * @returns {Object}
  */
 function find (obj, value) {
+  
+  let storage = [];
+
+  for (let key in obj) {
+    if (typeof obj[key] === 'object') {
+      const parent = find(obj[key], value);
+      if (parent !== null) {
+       storage.push(`${key}.${parent}`)
+      }
+    }
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+  
+  if (storage.length) {
+   return storage.length === 1 ? storage[0] : storage;
+  } else {
+    return null;
+  }
+  
+ 
+
 }
