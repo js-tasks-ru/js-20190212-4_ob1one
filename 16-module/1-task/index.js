ae65add83851633658b703d0a16ab25c996f8e27
Bogdan Kononov <b.b.kononov@gmail.com>
2019-05-08 09:42:47 +0300

    added solved taks of 16 module

--- 16-module/1-task/index.js
+++ 16-module/1-task/index.js
@@ -4,6 +4,22 @@
  * @returns {Promise}
  */
 function processGenerator(gen) {
+  return new Promise(resolve => {
+    function next(val) {
+      let result = gen.next(val);
+
+      if (result.done) {
+        resolve(result.value);
+        return;
+      }
+
+      if (result.value instanceof Promise) {
+        result.value.then(result => next(result));
+      }
+    }
+
+    next();
+  })
 }
 
 
